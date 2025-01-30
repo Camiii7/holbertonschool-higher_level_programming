4382aed0d9f5d6bc28dc71013ee73a7acbf3c2f3
Camiii7 <camilairina7@gmail.com>
2025-01-30 20:11:51 +0000

    Codigo modificado

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,9 +4,20 @@
 
 class Square:
     """Representa un cuadrado"""
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Inicializa un nuevo cuadrado.
+
+
         Args:
-            size (int): El tamaño del nuevo cuadrado.
+            size (int): El tamaño dellado del cuadrado es 0.
+
+
+        Raises:
+            TypeError: Si size no es un número entero.
+            ValueError: Si size es menor que 0.
         """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size

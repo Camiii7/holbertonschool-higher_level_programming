09b9147e2f597f2c5ec5ed2c0bbe0898a7944006
Camiii7 <camilairina7@gmail.com>
2025-02-04 17:16:38 +0000

    Print a list of integers in reverse

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Define una función para imprimir una lista de enteros en orden inverso.
+"""
+
+def print_reversed_list_integer(my_list=[]):
+    """
+    Imprime todos los enteros de una lista en orden inverso.
+
+    Args:
+        my_list (list): Lista de enteros.
+    """
+    if my_list:
+        for num in reversed(my_list):
+            print("{:d}".format(num))

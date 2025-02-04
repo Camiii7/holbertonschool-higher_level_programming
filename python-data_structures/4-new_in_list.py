f174d35b5c9075e6d02869d4beb39d37a99af4c1
Camiii7 <camilairina7@gmail.com>
2025-02-04 17:24:11 +0000

    Replace in a copy

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Define una funcion para reemplazar un elemento en una copia de una lisa"""
+
+
+def new_in_list(my_list, idx, element):
+    """ Reemplaza un elemento en una lista en una posicion espeifica sin modificar la origina
+
+    Args:
+        my_list (list): Lista original
+        idx (int): Indice donde se quiere reemplazar un elemento
+        element (any): Nuevo elemento a insertar
+
+    Return:
+        lista: Una nueva lista con el elemento reemplazado, o una copia de la original si idx es invalid
+    """
+    if idx < 0 or idx >= len(my_list):
+        return my_list[:]
+
+    new_list = my_list[:]
+    new_list[idx] = element
+    return new_list

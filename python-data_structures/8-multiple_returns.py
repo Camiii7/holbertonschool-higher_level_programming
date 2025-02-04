93128a678da4eacf7d557714e4caa77cde4ef7b3
Camiii7 <camilairina7@gmail.com>
2025-02-04 18:03:49 +0000

    MOre returns

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Define una funcion que devuelve la longitud de una cadena y su primer caracter"""
+
+
+def multiple_returns(sentence):
+    """
+    Retorna una tupla con la longitud de la cadena y su primer caracter
+
+    Args:
+        sentence (str): La cadena de texto
+
+    Returns:
+        tuple: Una tupla (longitud, primer_caracter)
+               Si la cadena esta vacia, devuelve (0, None)
+    """
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])

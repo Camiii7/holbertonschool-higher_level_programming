89f87111049c8a98ec85b26ceeb3169ff56476de
root <camilairina7@gmail.com>
2025-02-06 18:53:17 +0000

    Arreglo codeStyle

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -5,7 +5,6 @@
 class BaseGeometry:
     """Clase base para geometria"""
 
-
     def area(self):
         """Lanza una excepcion indicando que no esta implementado"""
         raise Exception("area() is not implemented")

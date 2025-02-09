482092d3879ed0936d4bdcd78a4956a27828289f
root <camilairina7@gmail.com>
2025-02-09 14:47:52 +0000

    Square 2

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Modulo que define la clase Square heredada de rectangle"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Clase Squiare que hereda de rectangle"""
+
+    def __init__(self, size):
+        """Inicializa el cuadrado con el tamao size"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """Calcula el area del cuadrado"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Devuelve una cadena representando el cuadrado"""
+        return f"[Square] {self.__sizde}/{self.__size}"

40fad1c931324240d54a9fa5cd0995fb74bd6f9b
root <camilairina7@gmail.com>
2025-02-09 14:36:48 +0000

    Square1

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Modulo que define la clase Square heredada de rectangle"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Squiare(Rectangle):
+    """Clase Squiare que hereda de rectangle"""
+
+    def __init__(self, size):
+        """Inicializa el cuadrado con el tamao size"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """Calcula el area del cuadrado"""
+        return self.__size * self.__size
